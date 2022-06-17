5e7f7427c13f2a65415e7f168e5c04f06f7cfe9e
maureen-james <manjax97@gmail.com>
2022-06-18 02:34:27 +0300

    add path url to search

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -4,5 +4,12 @@ from django.urls import path
 
 urlpatterns=[
     path('',views.welcome,name = 'welcome'),
-    
+    path(r'^about$',views.about, name = 'about'),
+    path(r'^contacts$',views.contacts, name = 'contacts'),
+    path(r'^signup', views.signup, name='signup'),
+    path(r'^login', LoginView.as_view(), name='login_url'),
+    path(r'^logout/', LogoutView.as_view(next_page='login_url'), name='logout_url'),
+    path(r'^post',views.post,name='post'),
+    path(r'^edit_profile/(?P<username>\w{0,50})',views.edit_profile, name='edit_profile'),
+    path(r'^search/', views.search_business, name='search'),
 ]
--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
 def welcome(request):
     return render(request, 'index.html')
+
+
+    
 
     
 
