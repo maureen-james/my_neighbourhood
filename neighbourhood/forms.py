e59efa946a6de09b2cd805b2774f9b15bdbc14e9
maureen-james <manjax97@gmail.com>
2022-06-18 02:50:18 +0300

    add forms

---
+++ neighbourhood/forms.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import User
+from django import forms
+from .models import Post,Neighbourhood,Comment,Business,Profile,Location
+
+
+
+
+
+class UpdateUser(forms.ModelForm):
+    email=forms.EmailField()
+    class Meta:
+        model=User
+        fields=['username', 'email']
+        
+class UpdateProfile(forms.ModelForm):
+    class Meta:
+        model=Profile
+        fields = ('first_name','last_name','bio','location','neighbourhood','email')
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ['user','neighborhood']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model=Post
+        fields=['title','post',]
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('comment',)
