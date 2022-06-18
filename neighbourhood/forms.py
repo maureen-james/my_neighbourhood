65dee3dcc7e9f73a9181f1e4e17dcd7711c6881b
maureen-james <manjax97@gmail.com>
2022-06-18 03:02:19 +0300

    add class model

--- neighbourhood/forms.py
+++ neighbourhood/forms.py
@@ -1,10 +1,21 @@
 from django.contrib.auth.models import User
 from django import forms
 from .models import Post,Neighbourhood,Comment,Business,Profile,Location
+from django.contrib.auth.forms import UserCreationForm
 
 
+class SignUpForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
 
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
 
+    email = forms.EmailField(max_length=254, required=True)
+
+    class Meta:
+        model = User
+        fields = ('email' ,'username','password1', 'password2', )
 
 class UpdateUser(forms.ModelForm):
     email=forms.EmailField()
--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -3,29 +3,68 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_photo = CloudinaryField("Profile Image")
-    bio = models.TextField(max_length=300)
-    location = models.CharField(max_length = 40)
-    email = models.EmailField(max_length=200)
-    contact = models.CharField(max_length=100, blank=True)
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     profile_photo = CloudinaryField("Profile Image")
+#     bio = models.TextField(max_length=300)
+#     location = models.CharField(max_length = 40)
+#     email = models.EmailField(max_length=200)
+#     contact = models.CharField(max_length=100, blank=True)
 
-    def save_profile(self):
-        self.save()
+#     def save_profile(self):
+#         self.save()
     
-    def update_profile(self):
+#     def update_profile(self):
+#         self.save()
+
+#     def delete_profile(self):
+#         self.delete()
+
+#     @classmethod
+#     def filter_by_id(cls, id):
+#         profile = Profile.objects.filter(user=id).first()
+#         return profile
+
+#     def __str__(self):
+#         return self.user.username
+
+class Neighbourhood(models.Model):
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=60)
+    # location = models.ForeignKey(Location, on_delete = models.CASCADE,null = True)
+    occupants = models.IntegerField(default=0)
+
+    def save_neighbourhood(self):
         self.save()
-
-    def delete_profile(self):
-        self.delete()
-
-    @classmethod
-    def filter_by_id(cls, id):
-        profile = Profile.objects.filter(user=id).first()
-        return profile
 
     def __str__(self):
+        return self.name
+
+    def create_neighbourhood(self):
+        self.save()
+
+    def delete_neighbourhood(self):
+        self.delete()
+
+    def update_neighbourhood(self):
+        self.save()
+
+    def update_occupants(self):
+        self.occupants += 1
+        self.save()
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE,related_name = 'profile')
+    profile_photo = CloudinaryField("Profile Image")
+    first_name = models.CharField(max_length = 50,null=True)
+    last_name = models.CharField(max_length = 50,null=True)
+    bio = models.TextField(max_length=300)
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    # location = models.ForeignKey(Location,on_delete = models.CASCADE,null=True)
+    email = models.EmailField(max_length=60, blank=True)
+    contact = models.CharField(max_length=100, blank=True)
+    
+    def __str__(self):
         return self.user.username
-
-
+        
