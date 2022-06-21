c60a219beb574ef3599d8dfaf7b7164d78cd56c8
maureen-james <manjax97@gmail.com>
2022-06-21 10:17:27 +0300

    modify login redirect

--- neighbour/settings.py
+++ neighbour/settings.py
@@ -158,6 +158,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_REDIRECT_URL = 'index'
+
+LOGOUT_REDIRECT_URL = 'index'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
